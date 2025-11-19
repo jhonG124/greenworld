20f2706f5581714bd99869c4ecca18446f3ed791
Jhon Gonzalez <jhon.gonzalez163@cide.edu.co>
2025-11-19 18:34:30 -0500

    Fix API URLs for production

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,8 @@
-const API_URL = process.env.REACT_APP_API_URL;
+let API_URL = process.env.REACT_APP_API_URL;
+
+// Fallback para desarrollo
+if (!API_URL) {
+  API_URL = "http://localhost:10000"; 
+}
 
 export default API_URL;
